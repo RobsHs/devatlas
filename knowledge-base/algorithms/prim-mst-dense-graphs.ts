73a846208fe3d06772933af0251b24b3fa2d5118
Freda <fredapacitan@gmail.com>
2025-09-22 18:52:34 +0700

    test(sorting): update prim-mst-dense-graphs [rev #238]

--- knowledge-base/algorithms/prim-mst-dense-graphs.ts
+++ knowledge-base/algorithms/prim-mst-dense-graphs.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - PRIM MST DENSE GRAPHS
- * Version Iteration: 106
+ * Version Iteration: 238
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_106 {
+export class Solution_238 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 5];
-    const result = Solution_106.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 37];
+    const result = Solution_238.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
