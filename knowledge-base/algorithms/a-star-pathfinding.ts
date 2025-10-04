3cd10e732ce5d6c4e09d30a35fd0d7cbb5a32406
Freda <fredapacitan@gmail.com>
2025-10-04 19:51:29 +0700

    test(sorting): update a-star-pathfinding [rev #442]

--- knowledge-base/algorithms/a-star-pathfinding.ts
+++ knowledge-base/algorithms/a-star-pathfinding.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - A STAR PATHFINDING
- * Version Iteration: 310
+ * Version Iteration: 442
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_310 {
+export class Solution_442 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 9];
-    const result = Solution_310.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 41];
+    const result = Solution_442.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
