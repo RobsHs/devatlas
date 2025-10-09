4236373becc6b5d59d58100b5e2f8b1c25663495
Freda <fredapacitan@gmail.com>
2025-10-09 16:55:51 +0700

    refactor(modules): update fast-fourier-transform [rev #508]

--- knowledge-base/algorithms/fast-fourier-transform.ts
+++ knowledge-base/algorithms/fast-fourier-transform.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - FAST FOURIER TRANSFORM
- * Version Iteration: 376
+ * Version Iteration: 508
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_376 {
+export class Solution_508 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 75];
-    const result = Solution_376.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 7];
+    const result = Solution_508.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
