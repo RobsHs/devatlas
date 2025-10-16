f8deac48d25bc8388e097f85c71a3e88702fe551
Freda <fredapacitan@gmail.com>
2025-10-16 09:55:01 +0700

    test(graphs): update coin-change-fewest [rev #604]

--- knowledge-base/algorithms/coin-change-fewest.ts
+++ knowledge-base/algorithms/coin-change-fewest.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - COIN CHANGE FEWEST
- * Version Iteration: 472
+ * Version Iteration: 604
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_472 {
+export class Solution_604 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 71];
-    const result = Solution_472.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 3];
+    const result = Solution_604.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
