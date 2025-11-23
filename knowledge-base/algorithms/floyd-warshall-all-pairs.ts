c1440fa80e998c02ead929a1c6b348d4e7d4a677
Freda <fredapacitan@gmail.com>
2025-11-23 18:58:06 +0700

    docs(readme): update floyd-warshall-all-pairs [rev #1159]

--- knowledge-base/algorithms/floyd-warshall-all-pairs.ts
+++ knowledge-base/algorithms/floyd-warshall-all-pairs.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - FLOYD WARSHALL ALL PAIRS
- * Version Iteration: 1027
+ * Version Iteration: 1159
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_1027 {
+export class Solution_1159 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 26];
-    const result = Solution_1027.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 58];
+    const result = Solution_1159.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
