0395e33f51894e3e3d26f659b0b68db5613ae398
Freda <fredapacitan@gmail.com>
2025-12-22 13:27:11 +0700

    feat(system-design): update levenshtein-edit-distance [rev #1531]

--- knowledge-base/algorithms/levenshtein-edit-distance.ts
+++ knowledge-base/algorithms/levenshtein-edit-distance.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - LEVENSHTEIN EDIT DISTANCE
- * Version Iteration: 1399
+ * Version Iteration: 1531
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_1399 {
+export class Solution_1531 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 98];
-    const result = Solution_1399.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 30];
+    const result = Solution_1531.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
