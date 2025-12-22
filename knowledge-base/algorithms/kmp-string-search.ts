b438e180d86227630aa8b6013462723846656c0a
Freda <fredapacitan@gmail.com>
2025-12-22 18:34:00 +0700

    feat(system-design): update kmp-string-search [rev #1543]

--- knowledge-base/algorithms/kmp-string-search.ts
+++ knowledge-base/algorithms/kmp-string-search.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - KMP STRING SEARCH
- * Version Iteration: 1411
+ * Version Iteration: 1543
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_1411 {
+export class Solution_1543 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 10];
-    const result = Solution_1411.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 42];
+    const result = Solution_1543.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
