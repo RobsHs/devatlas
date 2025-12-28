5497a1400496ef3a29a63a4f11185c422067088a
Freda <fredapacitan@gmail.com>
2025-12-28 14:31:21 +0700

    test(dp): update knapsack-01-dynamic-programming [rev #1615]

--- knowledge-base/algorithms/knapsack-01-dynamic-programming.ts
+++ knowledge-base/algorithms/knapsack-01-dynamic-programming.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - KNAPSACK 01 DYNAMIC PROGRAMMING
- * Version Iteration: 1483
+ * Version Iteration: 1615
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_1483 {
+export class Solution_1615 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 82];
-    const result = Solution_1483.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 14];
+    const result = Solution_1615.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
