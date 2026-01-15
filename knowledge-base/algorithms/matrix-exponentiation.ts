a2f4bb584686e518c71bbfa48b816d862bcdd3d8
Freda <fredapacitan@gmail.com>
2026-01-15 13:50:32 +0700

    feat(api): update matrix-exponentiation [rev #1873]

--- knowledge-base/algorithms/matrix-exponentiation.ts
+++ knowledge-base/algorithms/matrix-exponentiation.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - MATRIX EXPONENTIATION
- * Version Iteration: 1741
+ * Version Iteration: 1873
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_1741 {
+export class Solution_1873 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 40];
-    const result = Solution_1741.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 72];
+    const result = Solution_1873.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
