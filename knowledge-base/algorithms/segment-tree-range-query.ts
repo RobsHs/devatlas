98d98d1f2281eea18c988a451a1767e70b308001
Freda <fredapacitan@gmail.com>
2026-01-16 21:21:19 +0700

    test(dp): update segment-tree-range-query [rev #1897]

--- knowledge-base/algorithms/segment-tree-range-query.ts
+++ knowledge-base/algorithms/segment-tree-range-query.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - SEGMENT TREE RANGE QUERY
- * Version Iteration: 1765
+ * Version Iteration: 1897
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_1765 {
+export class Solution_1897 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 64];
-    const result = Solution_1765.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 96];
+    const result = Solution_1897.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
