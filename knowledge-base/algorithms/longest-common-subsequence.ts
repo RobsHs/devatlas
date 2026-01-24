f8849f90abbc54a3b40f33c1713c9b84e590b233
Freda <fredapacitan@gmail.com>
2026-01-24 16:10:02 +0700

    feat(dsa): update longest-common-subsequence [rev #2014]

--- knowledge-base/algorithms/longest-common-subsequence.ts
+++ knowledge-base/algorithms/longest-common-subsequence.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - LONGEST COMMON SUBSEQUENCE
- * Version Iteration: 1882
+ * Version Iteration: 2014
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_1882 {
+export class Solution_2014 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 81];
-    const result = Solution_1882.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 13];
+    const result = Solution_2014.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
