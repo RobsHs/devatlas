0c3ba344795019dec6b90f291aec2ce2958d4648
Freda <fredapacitan@gmail.com>
2026-01-27 21:12:01 +0700

    test(e2e): update sieve-eratosthenes-segmented [rev #2053]

--- knowledge-base/algorithms/sieve-eratosthenes-segmented.ts
+++ knowledge-base/algorithms/sieve-eratosthenes-segmented.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - SIEVE ERATOSTHENES SEGMENTED
- * Version Iteration: 1921
+ * Version Iteration: 2053
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_1921 {
+export class Solution_2053 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 20];
-    const result = Solution_1921.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 52];
+    const result = Solution_2053.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
