260445b29852581644e15ad2e208336c4da6a327
Freda <fredapacitan@gmail.com>
2026-02-10 09:39:19 +0700

    test(graphs): update trapping-rain-water-two-pointers [rev #2239]

--- knowledge-base/algorithms/trapping-rain-water-two-pointers.ts
+++ knowledge-base/algorithms/trapping-rain-water-two-pointers.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - TRAPPING RAIN WATER TWO POINTERS
- * Version Iteration: 2107
+ * Version Iteration: 2239
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_2107 {
+export class Solution_2239 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 6];
-    const result = Solution_2107.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 38];
+    const result = Solution_2239.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
