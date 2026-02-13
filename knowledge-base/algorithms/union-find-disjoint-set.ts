224c9e2beaa3d76eca00a699dfe234cf9644f741
Freda <fredapacitan@gmail.com>
2026-02-13 12:08:12 +0700

    refactor(interfaces): update union-find-disjoint-set [rev #2308]

--- knowledge-base/algorithms/union-find-disjoint-set.ts
+++ knowledge-base/algorithms/union-find-disjoint-set.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - UNION FIND DISJOINT SET
- * Version Iteration: 2176
+ * Version Iteration: 2308
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_2176 {
+export class Solution_2308 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 75];
-    const result = Solution_2176.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 7];
+    const result = Solution_2308.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
