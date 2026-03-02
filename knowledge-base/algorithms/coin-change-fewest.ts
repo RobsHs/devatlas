11e644d975c66fe3e704aca83d736c4862d8f042
Freda <fredapacitan@gmail.com>
2026-03-02 19:20:26 +0700

    feat(api): update coin-change-fewest [rev #2584]

--- knowledge-base/algorithms/coin-change-fewest.ts
+++ knowledge-base/algorithms/coin-change-fewest.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - COIN CHANGE FEWEST
- * Version Iteration: 2452
+ * Version Iteration: 2584
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_2452 {
+export class Solution_2584 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 51];
-    const result = Solution_2452.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 83];
+    const result = Solution_2584.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
