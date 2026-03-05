2be6c89d307ad5f9b0e754041dbe1be7535588a5
Freda <fredapacitan@gmail.com>
2026-03-05 15:31:35 +0700

    test(sorting): update sudoku-solver-constraint [rev #2638]

--- knowledge-base/algorithms/sudoku-solver-constraint.ts
+++ knowledge-base/algorithms/sudoku-solver-constraint.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - SUDOKU SOLVER CONSTRAINT
- * Version Iteration: 2506
+ * Version Iteration: 2638
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_2506 {
+export class Solution_2638 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 5];
-    const result = Solution_2506.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 37];
+    const result = Solution_2638.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
