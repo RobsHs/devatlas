c2ef73fdd001b904956a14623ec5db5b39ca2854
Freda <fredapacitan@gmail.com>
2026-03-08 13:00:25 +0700

    test(sorting): update fibonacci-heap [rev #2662]

--- knowledge-base/algorithms/fibonacci-heap.ts
+++ knowledge-base/algorithms/fibonacci-heap.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - FIBONACCI HEAP
- * Version Iteration: 2530
+ * Version Iteration: 2662
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_2530 {
+export class Solution_2662 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 29];
-    const result = Solution_2530.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 61];
+    const result = Solution_2662.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
