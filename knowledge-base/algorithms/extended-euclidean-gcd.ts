b40cb431d82baa88397142b3885db1162d445f22
Freda <fredapacitan@gmail.com>
2026-03-23 20:09:41 +0700

    refactor(typings): update extended-euclidean-gcd [rev #2890]

--- knowledge-base/algorithms/extended-euclidean-gcd.ts
+++ knowledge-base/algorithms/extended-euclidean-gcd.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - EXTENDED EUCLIDEAN GCD
- * Version Iteration: 2758
+ * Version Iteration: 2890
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_2758 {
+export class Solution_2890 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 57];
-    const result = Solution_2758.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 89];
+    const result = Solution_2890.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
