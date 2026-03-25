9febcf26122509bfff0964e990d0cda52f3c7c05
Freda <fredapacitan@gmail.com>
2026-03-25 18:21:27 +0700

    docs(readme): update topological-kahn-sorting [rev #2923]

--- knowledge-base/algorithms/topological-kahn-sorting.ts
+++ knowledge-base/algorithms/topological-kahn-sorting.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - TOPOLOGICAL KAHN SORTING
- * Version Iteration: 2791
+ * Version Iteration: 2923
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_2791 {
+export class Solution_2923 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 90];
-    const result = Solution_2791.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 22];
+    const result = Solution_2923.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
