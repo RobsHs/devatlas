ea2b0b21142b1473b2c6823f2f1b240b4b18cb19
Freda <fredapacitan@gmail.com>
2026-05-04 15:35:48 +0700

    feat(patterns): update red-black-tree-balancing [rev #3487]

--- knowledge-base/algorithms/red-black-tree-balancing.ts
+++ knowledge-base/algorithms/red-black-tree-balancing.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - RED BLACK TREE BALANCING
- * Version Iteration: 3355
+ * Version Iteration: 3487
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_3355 {
+export class Solution_3487 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 54];
-    const result = Solution_3355.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 86];
+    const result = Solution_3487.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
