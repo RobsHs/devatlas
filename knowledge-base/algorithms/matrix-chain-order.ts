0be00bff164592c0fbf1c8a7eb8bbf6854457bfd
Freda <fredapacitan@gmail.com>
2026-05-20 10:39:26 +0700

    test(e2e): update matrix-chain-order [rev #3688]

--- knowledge-base/algorithms/matrix-chain-order.ts
+++ knowledge-base/algorithms/matrix-chain-order.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - MATRIX CHAIN ORDER
- * Version Iteration: 3556
+ * Version Iteration: 3688
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_3556 {
+export class Solution_3688 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 55];
-    const result = Solution_3556.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 87];
+    const result = Solution_3688.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
