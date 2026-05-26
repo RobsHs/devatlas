c851d1bad59e16aeab05078e77eafe64f4121b72
Freda <fredapacitan@gmail.com>
2026-05-26 11:36:52 +0700

    test(graphs): update n-queens-backtracking [rev #3781]

--- knowledge-base/algorithms/n-queens-backtracking.ts
+++ knowledge-base/algorithms/n-queens-backtracking.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - N QUEENS BACKTRACKING
- * Version Iteration: 3649
+ * Version Iteration: 3781
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_3649 {
+export class Solution_3781 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 48];
-    const result = Solution_3649.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 80];
+    const result = Solution_3781.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
