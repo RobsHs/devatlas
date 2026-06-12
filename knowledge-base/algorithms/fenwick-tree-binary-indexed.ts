2d044e5713cfe69d1b87d2f000074c84cfddf651
Freda <fredapacitan@gmail.com>
2026-06-12 10:25:50 +0700

    test(sorting): update fenwick-tree-binary-indexed [rev #4054]

--- knowledge-base/algorithms/fenwick-tree-binary-indexed.ts
+++ knowledge-base/algorithms/fenwick-tree-binary-indexed.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - FENWICK TREE BINARY INDEXED
- * Version Iteration: 3922
+ * Version Iteration: 4054
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_3922 {
+export class Solution_4054 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 21];
-    const result = Solution_3922.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 53];
+    const result = Solution_4054.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
