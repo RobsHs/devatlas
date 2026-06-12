b7db61070a79807cb0a863b3f923a249f3a7da5a
Freda <fredapacitan@gmail.com>
2026-06-12 20:09:58 +0700

    perf(io): update fast-fourier-transform [rev #4072]

--- knowledge-base/algorithms/fast-fourier-transform.ts
+++ knowledge-base/algorithms/fast-fourier-transform.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - FAST FOURIER TRANSFORM
- * Version Iteration: 3940
+ * Version Iteration: 4072
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_3940 {
+export class Solution_4072 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 39];
-    const result = Solution_3940.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 71];
+    const result = Solution_4072.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
