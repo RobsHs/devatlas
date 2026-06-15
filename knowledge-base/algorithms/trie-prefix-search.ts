595264df985e65f1c776dac9432b72aebc7cd670
Freda <fredapacitan@gmail.com>
2026-06-15 11:34:40 +0700

    docs(readme): update trie-prefix-search [rev #4099]

--- knowledge-base/algorithms/trie-prefix-search.ts
+++ knowledge-base/algorithms/trie-prefix-search.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - TRIE PREFIX SEARCH
- * Version Iteration: 3967
+ * Version Iteration: 4099
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_3967 {
+export class Solution_4099 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 66];
-    const result = Solution_3967.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 98];
+    const result = Solution_4099.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
