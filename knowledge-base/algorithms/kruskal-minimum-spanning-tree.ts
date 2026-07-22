c30cef43e616e5729debc6633d897d322772b59e
Freda <fredapacitan@gmail.com>
2026-07-22 17:42:13 +0700

    test(graphs): update kruskal-minimum-spanning-tree [rev #4681]

--- knowledge-base/algorithms/kruskal-minimum-spanning-tree.ts
+++ knowledge-base/algorithms/kruskal-minimum-spanning-tree.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - KRUSKAL MINIMUM SPANNING TREE
- * Version Iteration: 4549
+ * Version Iteration: 4681
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_4549 {
+export class Solution_4681 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 48];
-    const result = Solution_4549.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 80];
+    const result = Solution_4681.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
