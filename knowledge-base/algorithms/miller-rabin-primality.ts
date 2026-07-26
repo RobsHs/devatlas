669e4d8d9475a33e04e1b0da6151b33e6ee59b62
Freda <fredapacitan@gmail.com>
2026-07-26 16:57:28 +0700

    test(e2e): update miller-rabin-primality [rev #4735]

--- knowledge-base/algorithms/miller-rabin-primality.ts
+++ knowledge-base/algorithms/miller-rabin-primality.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - MILLER RABIN PRIMALITY
- * Version Iteration: 4603
+ * Version Iteration: 4735
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_4603 {
+export class Solution_4735 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 2];
-    const result = Solution_4603.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 34];
+    const result = Solution_4735.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
