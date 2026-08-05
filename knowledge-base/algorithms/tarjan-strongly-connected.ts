e9bad020585f6dec35f29146cc16b91d07f9154e
Freda <fredapacitan@gmail.com>
2026-08-05 15:14:30 +0700

    test(dp): update tarjan-strongly-connected [rev #4900]

--- knowledge-base/algorithms/tarjan-strongly-connected.ts
+++ knowledge-base/algorithms/tarjan-strongly-connected.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - TARJAN STRONGLY CONNECTED
- * Version Iteration: 4768
+ * Version Iteration: 4900
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_4768 {
+export class Solution_4900 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 67];
-    const result = Solution_4768.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 99];
+    const result = Solution_4900.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
