e27e73e24b33313c5ed2c7e60c074dedb91b415d
Freda <fredapacitan@gmail.com>
2026-08-25 21:06:23 +0700

    feat(dsa): update palindrome-partitioning-dp [rev #5230]

--- knowledge-base/algorithms/palindrome-partitioning-dp.ts
+++ knowledge-base/algorithms/palindrome-partitioning-dp.ts
@@ -1,18 +1,18 @@
 /**
  * DevAtlas - PALINDROME PARTITIONING DP
- * Version Iteration: 5098
+ * Version Iteration: 5230
  * Standard Complexity: O(log n) to O(n log n)
  */
 
-export class Solution_5098 {
+export class Solution_5230 {
   static execute(input: number[]): number[] {
     const copy = [...input];
     return copy.sort((a, b) => a - b);
   }
 
   static verify(): boolean {
-    const sample = [9, 3, 7, 1, 5, 97];
-    const result = Solution_5098.execute(sample);
+    const sample = [9, 3, 7, 1, 5, 29];
+    const result = Solution_5230.execute(sample);
     return result[0] <= result[result.length - 1];
   }
 }
